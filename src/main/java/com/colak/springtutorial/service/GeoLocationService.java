5e6854ad6515b3ab9a05fe674a42c790f43d847c
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-30 08:20:26 +0300

    remove redundant fields

--- src/main/java/com/colak/springtutorial/service/GeoLocationService.java
+++ src/main/java/com/colak/springtutorial/service/GeoLocationService.java
@@ -10,19 +10,16 @@ import org.springframework.data.geo.Metrics;
 import org.springframework.data.geo.Point;
 import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.GeoOperations;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
 
 @Service
 @RequiredArgsConstructor
 public class GeoLocationService {
 
     private final GeoOperations<String, String> geoOperations;
-    private final StringRedisTemplate redisTemplate;
     public static final String vehicleLocation = "vehicle_location";
 
     public void add(VehicleLocationRequest request) {
@@ -31,9 +28,12 @@ public class GeoLocationService {
     }
 
     public List<VehicleLocationResponse> findNearestVehicles(Double longitude, Double latitude, int km) {
-
-        RedisGeoCommands.GeoRadiusCommandArgs args = RedisGeoCommands.GeoRadiusCommandArgs.newGeoRadiusArgs().includeCoordinates()
-                .includeDistance().sortAscending().limit(10);
+        RedisGeoCommands.GeoRadiusCommandArgs args = RedisGeoCommands.GeoRadiusCommandArgs
+                .newGeoRadiusArgs()
+                .includeCoordinates()
+                .includeDistance()
+                .sortAscending()
+                .limit(10);
 
         Circle circle = new Circle(new Point(longitude, latitude), new Distance(km, Metrics.KILOMETERS));
         GeoResults<RedisGeoCommands.GeoLocation<String>> response = geoOperations.radius(vehicleLocation, circle, args);
@@ -41,20 +41,18 @@ public class GeoLocationService {
         List<VehicleLocationResponse> vehicleLocationResponses = new ArrayList<>();
         assert response != null;
         response.getContent().forEach(data -> {
-
             List<String> hashList = geoOperations.hash(vehicleLocation, data.getContent().getName());
             assert hashList != null;
 
-            vehicleLocationResponses.add(VehicleLocationResponse.builder()
+            VehicleLocationResponse vehicleLocationResponse = VehicleLocationResponse.builder()
                     .vehicleName(data.getContent().getName())
                     .averageDistance(data.getDistance().toString())
                     .point(data.getContent().getPoint())
                     .hash(hashList.getFirst())
-                    .build());
-
+                    .build();
+            vehicleLocationResponses.add(vehicleLocationResponse);
         });
 
         return vehicleLocationResponses;
     }
-
 }
